9f703603ef1d3d662551f27109408e2cd2dc4cfc
enm10k <enm10k@gmail.com>
2024-03-26 15:18:59 +0900

    sora-cpp-sdk の feature/onevpl-h265-m122 ブランチを参照してビルドする

--- base.py
+++ base.py
@@ -94,6 +94,48 @@ def add_path(path: str, is_after=False):
     else:
         os.environ["PATH"] = path + PATH_SEPARATOR + os.environ["PATH"]
 
+def gh_run_download(
+    repo: str,
+    filename: str,
+    output_dir: str,
+    branch: Optional[str] = None,
+    run_id: Optional[str] = None,
+):
+    # branch が指定されたら、そのブランチの最新の artifact を参照する
+    if branch:
+        run_id = cmdcap(
+            [
+                "gh",
+                "-R",
+                repo,
+                "run",
+                "list",
+                "-b",
+                branch,
+                "-s",
+                "success",
+                "--json",
+                "databaseId",
+                "--jq",
+                ".[0].databaseId",
+            ]
+        )
+
+    output_path = os.path.join(output_dir, filename)
+
+    if os.path.exists(output_path):
+        return output_path
+
+    try:
+        cmd(["gh", "-R", repo, "run", "download", run_id, "-n", filename, "-D", output_dir])
+    except Exception:
+        # ゴミを残さないようにする
+        if os.path.exists(output_path):
+            os.remove(output_path)
+        raise
+
+    return output_path
+
 
 def download(url: str, output_dir: Optional[str] = None, filename: Optional[str] = None) -> str:
     if filename is None:
@@ -464,9 +506,15 @@ def install_sora(version, source_dir, install_dir, platform: str):
     win = platform.startswith("windows_")
     filename = f'sora-cpp-sdk-{version}_{platform}.{"zip" if win else "tar.gz"}'
     rm_rf(os.path.join(source_dir, filename))
-    archive = download(
-        f"https://github.com/shiguredo/sora-cpp-sdk/releases/download/{version}/{filename}",
-        output_dir=source_dir,
+    # archive = download(
+    #     f"https://github.com/shiguredo/sora-cpp-sdk/releases/download/{version}/{filename}",
+    #     output_dir=source_dir,
+    # )
+    archive = gh_run_download(
+        "shiguredo/sora-cpp-sdk",
+        filename,
+        source_dir,
+        branch="feature/onevpl-h265-m122",
     )
     rm_rf(os.path.join(install_dir, "sora"))
     extract(archive, output_dir=install_dir, output_dirname="sora")
@@ -583,9 +631,15 @@ def install_boost(version, source_dir, install_dir, sora_version, platform: str)
         f'boost-{version}_sora-cpp-sdk-{sora_version}_{platform}.{"zip" if win else "tar.gz"}'
     )
     rm_rf(os.path.join(source_dir, filename))
-    archive = download(
-        f"https://github.com/shiguredo/sora-cpp-sdk/releases/download/{sora_version}/{filename}",
-        output_dir=source_dir,
+    # archive = download(
+    #     f"https://github.com/shiguredo/sora-cpp-sdk/releases/download/{sora_version}/{filename}",
+    #     output_dir=source_dir,
+    # )
+    archive = gh_run_download(
+        "shiguredo/sora-cpp-sdk",
+        filename,
+        source_dir,
+        branch="feature/onevpl-h265-m122",
     )
     rm_rf(os.path.join(install_dir, "boost"))
     extract(archive, output_dir=install_dir, output_dirname="boost")
